53459b65f47d47fad2634b8f305498a44e9ab7c0
SantiagoComas07 <santiagocomas07d@gmail.com>
2025-01-13 06:50:57 -0500

    Quinta edición, se agrego la sesion 'skills' y se aplicaron caracteristicas responsive al 'header'

---
+++ presentacion.js
@@ -0,0 +1,16 @@
+let nav = document.querySelector(".contenedor-listas");
+let abrir = document.querySelector("#btn-abrir");
+let cerrar = document.querySelector("#btn-cerrar");
+
+
+abrir. addEventListener("click", ()=>{
+    console.log("Abrir")
+    nav.classList.add("visible");
+
+})
+
+cerrar. addEventListener("click", ()=>{
+    console.log("Cerrar")
+    nav.classList.remove("visible");
+
+})
